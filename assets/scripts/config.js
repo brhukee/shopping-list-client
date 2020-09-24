fdd1bf0ac9539ca0b0e347e3b0bc1dfb6528bb52
Brandon Hukee <brandonhukee@Brandons-MacBook-Air.local>
2020-09-24 10:08:18 -0600

    Removed / from API Url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://calm-harbor-38242.herokuapp.com/',
+  production: 'https://calm-harbor-38242.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
